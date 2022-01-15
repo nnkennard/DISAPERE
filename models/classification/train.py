e592d8067cd4363d787a138c768be08df5ff5f29
nnayak_umass_edu <nnayak_umass_edu@node61.maas>
2022-01-15 02:31:52 +0000

    works but needs options

--- models/classification/train.py
+++ models/classification/train.py
@@ -1,89 +1,58 @@
 import pickle
 import csv
 import collections
-#import config
-#import dataset
 import engine
 import torch
 import pandas as pd
 import torch.nn as nn
 import numpy as np
 import sys
-import json
 import transformers
 
-#from model import BERTBaseUncased
-from sklearn import model_selection
 from sklearn import metrics
 from transformers import AdamW
 from transformers import get_linear_schedule_with_warmup
 
-import logging
 import classification_lib
 from classification_lib import Config
-
-logging.basicConfig(level=logging.ERROR)
-
-#with open("labels.json", 'r') as f:
-#  label_map = json.load(f)
-
-
-def preprocess(filename, label):
-  dicts = []
-  with open(filename, 'r') as f:
-    r = csv.DictReader(f)
-    for row in r:
-      label_val = row[label]
-      dicts.append({
-          'sentence': row["sentence"],
-          label: row[label],
-          'ENCODE_CAT': label_map[label].index(row[label])
-      })
-  return pd.DataFrame.from_dict(dicts)
-
-
-def monitor_metrics(outputs, targets):
-  if targets is None:
-    return {}
-  outputs = torch.argmax(outputs, dim=0).cpu().detach().numpy()
-  targets = targets.cpu().detach().numpy()
-  accuracy = metrics.accuracy_score(targets, outputs)
-  return accuracy
-
-
-#def process_dataset(df, batch_size, num_workers):
-#  df = df.reset_index(drop=True)
-#  this_dataset = classification_lib.BERTDataset(review=df.sentence.values,
-#                                                target=df.ENCODE_CAT.values)
-#  data_loader = torch.utils.data.DataLoader(this_dataset,
-#                                            batch_size=batch_size,
-#                                            num_workers=num_workers)
-#  return data_loader
 
 
 def get_num_train_steps(sample_list):
   return int(len(sample_list) / Config.TRAIN_BATCH_SIZE * Config.EPOCHS)
 
 
-def run():
+def main():
 
-  data_file, label = sys.argv[1:3]
+  data_file, task = sys.argv[1:3]
 
   with open("data/label_map.pkl", 'rb') as f:
     label_map = pickle.load(f)
 
   label2int = label_map["coarse"]
 
-  model_path = "models2/" + label + "_best.pt"
+  model_path = "models/" + task + "_best.pt"
   samples = collections.defaultdict(list)
+  dataframe_builders = collections.defaultdict(list)
   with open(data_file, 'r', encoding='utf8') as fIn:
     reader = csv.DictReader(fIn, delimiter='\t')
     for row in reader:
-      if not row['dataset'] == 'coarse':
+      if not row['dataset'] == task:
         continue
       label_id = label2int[row['label']]
-      samples[row['split']].append(
-          (row['sentence1'], label_id))
+      samples[row['split']].append((row['sentence1'], label_id))
+      dataframe_builders[row['split']].append({
+          'sentence': row['sentence1'],
+          'task': task,
+          'label': row['label'],
+          'encoded': float(label_id)
+      })
+
+  dataframes = {
+      split: pd.DataFrame.from_dict(dicts)
+      for split, dicts in dataframe_builders.items()
+  }
+
+  pd.set_option('display.max_columns', None)
 
   dataloaders = {}
   for subset, sample_list in samples.items():
@@ -96,9 +65,8 @@ def run():
 
     dataset = classification_lib.BERTDataset(sample_list)
     dataloaders[subset] = torch.utils.data.DataLoader(dataset,
-                                     batch_size=batch_size,
-                                     num_workers=num_workers)
-
+                                                      batch_size=batch_size,
+                                                      num_workers=num_workers)
 
   device = torch.device(Config.DEVICE)
   model = classification_lib.BERTBaseUncased(len(label2int))
@@ -132,7 +100,6 @@ def run():
   best_val_epoch = None
 
   best_accuracy = 0
-  print("HERE")
   for epoch in range(Config.EPOCHS):
     engine.train_fn(dataloaders["train"], model, optimizer, device, scheduler,
                     epoch)
@@ -158,14 +125,12 @@ def run():
 
     result_df = pd.DataFrame.from_dict(result_df_dicts)
 
-    #final_df = pd.concat([dataframes[subset], result_df], axis=1)
-    final_df = result_df
-    #for i in final_df.itertuples():
-    #  assert i.ENCODE_CAT == i.target
+    final_df = pd.concat([dataframes[subset], result_df], axis=1)
+    #assert all(i.encoded == i.target for i in final_df.itertuples())
 
-    result_file = "results2/" + subset + "_" + label + ".csv"
+    result_file = "results/" + subset + "_" + task + ".csv"
     final_df.to_csv(result_file)
 
 
 if __name__ == "__main__":
-  run()
+  main()
